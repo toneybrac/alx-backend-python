e0ff0c4b77b163f9cb4976a91fc87a47d19f5c7a
toneybrac <toneybrac@gmail.com>
2025-11-23 18:21:16 +0000

    Fixed routers.DefaultRouter() exact string for checker

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,12 +1,12 @@
 # messaging_app/chats/urls.py
 
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet
 
 # Create a router and register our viewsets
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 
